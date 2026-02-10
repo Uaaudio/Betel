90732f6b1442c6f4c448cbc8e00330932a09ecf5
Wendell <wendelljesusprofissional@gmail.com>
2026-02-10 13:12:31 -0300

    pronto pro deploy

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,21 +1,32 @@
 const Sequelize = require("sequelize");
+require('dotenv').config();
 
-
-const Connection = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASS,{
-
-    dialect: "mysql",
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT
-
-});
+// Se existir DB_URL, usamos ela. Caso contrário, usamos as variáveis separadas.
+const Connection = process.env.DB_URL 
+    ? new Sequelize(process.env.DB_URL, {
+        dialect: "mysql",
+        timezone: "-03:00",
+        logging: false,
+        dialectOptions: {
+            ssl: {
+                rejectUnauthorized: false
+            }
+        }
+    })
+    : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+        dialect: "mysql",
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT || 3306,
+        timezone: "-03:00",
+        logging: false
+    });
 
 Connection.authenticate()
-    .then(()=>{
-        console.log("Banco conectado com sucesso!")
-    }).catch((error)=>{
+    .then(() => {
+        console.log("🚀 Banco conectado com sucesso!");
+    }).catch((error) => {
         console.log("falha ao conectar ao banco de dados");
         console.log(error);
     });
 
-
-module.exports = Connection
+module.exports = Connection;
