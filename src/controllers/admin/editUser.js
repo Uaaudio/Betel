aaf80db7ea58d9c4a0dc3b477d16dcfad4aa14b6
Wendell <wendelljesusprofissional@gmail.com>
2026-02-10 22:06:47 -0300

    criação da função de editar usuario e de ver patios

--- src/controllers/admin/editUser.js
+++ src/controllers/admin/editUser.js
@@ -1,5 +1,5 @@
 
-const User = require("../../models");
+const User = require("../../models/user");
 const bcrypt = require("bcrypt");
 
 async function editUser(req,res){
--- src/controllers/admin/seePatios.js
+++ src/controllers/admin/seePatios.js
@@ -1,4 +1,4 @@
-const { where } = require("sequelize");
+
 const patio = require("../../models/patio");
 
 
